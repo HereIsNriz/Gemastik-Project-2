04f649e4d09b8a0432f9021fcfb09abf55ca7ac4
Nriz <here.is.nriz@gmail.com>
2026-07-08 22:58:38 +0700

    Play and Exit Button
    - Set the tittle of the game to "Smash The Trash"
    - Add a play button
    - Add an exit button

--- Gemastik Project 2/Assets/Scripts/MenuManager.cs
+++ Gemastik Project 2/Assets/Scripts/MenuManager.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class MenuManager : MonoBehaviour
 {
+    private float m_soundDelay = 0.2f;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void PressPlayButton()
+    {
+        // SFX
+        StartCoroutine(PlayButtonSound());
+    }
+    private IEnumerator PlayButtonSound()
+    {
+        yield return new WaitForSeconds(m_soundDelay);
+        SceneManager.LoadScene(1);
+    }
+    public void PressExitButton()
+    {
+        // SFX
+        StartCoroutine(ExitButtonSound());
+    }
+    private IEnumerator ExitButtonSound()
+    {
+        yield return new WaitForSeconds(m_soundDelay);
+        //Application.Quit();
+        EditorApplication.ExitPlaymode();
     }
 }
